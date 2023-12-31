9b9f26206a8e9c32b9514e73acc1eb8b957e0a0c
TomStlau <tommy.stlau@gmail.com>
2023-12-30 22:26:03 -0500

    Added 100 posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,10 @@
 
 user = User.where(email: "tommy.stlau@gmail.com").first_or_initialize
 user.update!(password: "password", password_confirmation: "password")
+
+100.times do |i|
+  BlogPost.where(title: "Blog Post #{i+1}").first_or_create(
+    content: "This is the content for blog post #{i+1}",
+    published_at: 1.year.ago
+  )
+end
